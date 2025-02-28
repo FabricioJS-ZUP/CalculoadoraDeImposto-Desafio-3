45191ee2467e31335843bfeafc1c8753e5889d80
Fabricio John Sullivan <fabricio.sullivan@zup.com.br>
2025-02-28 15:01:32 -0300

    Inserting the base file
    chore: Project base with dependencies: spring security, spring data Jpa, Lombok, PostgreSQL Driver, Mokito and Jwt

---
+++ Tax-Calculator/src/test/java/br/com/catalisa/zup/Tax/Calculator/TaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalisa.zup.Tax.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
